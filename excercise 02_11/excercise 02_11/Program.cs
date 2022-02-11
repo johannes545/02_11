6e6935768aa43e3deb7688b91fe3a921503f3564
johannes545 <Johannes545@gmail.com>
2022-02-11 17:54:50 +0100

    clean plate

---
+++ excercise 02_11/excercise 02_11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace excercise_02_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
